5602889b3c2100f9034c917585c35908c50960a0
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-12 16:51:40 +0700

    Editing dependencies :arrow_up:

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React from 'react';
+import { useEthers } from '@usedapp/core';
+
+import styles from './styles';
 
 const App = () => {
-  return <p>Hello, Web3!</p>
-}
+  return <p>Hello, Web3!</p>;
+};
 
 export default App;
