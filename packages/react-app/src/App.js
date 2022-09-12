bbe523c997f38ee3cc177b73c7c2e1dbcae4ee02
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-13 01:37:23 +0700

    Configuration :wrench:

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { useEthers } from '@usedapp/core';
 
 import styles from './styles';
+import { uniswapLogo } from './assets';
 
 const App = () => {
-  return <p>Hello, Web3!</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img
+            src={uniswapLogo}
+            alt="uniswap logo"
+            className="w-16 h-16 object-contain"
+          />
+          WalletButton
+        </header>
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,11 @@
-import { Goerli } from "@usedapp/core";
+import { Goerli } from '@usedapp/core';
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = '0x36c6b3089fAA8f2717C9c63Ca73B7784c88029BE';
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]:
+      'https://eth-goerli.g.alchemy.com/v2/wLSom8H0ho6-67-JRHFeFBx0kE0N0lFm',
   },
 };
