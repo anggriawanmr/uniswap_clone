f1709e9b59e1d36e43945eb50b9caa8e0535e489
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-13 02:34:26 +0700

    Adding components :sparkles:

---
+++ packages/react-app/src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from '../styles';
+import { ethereumLogo } from '../assets';
+
+const Loader = () => {
+  return <div>Loader</div>;
+};
+
+export default Loader;
