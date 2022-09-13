8daae78ba77af88d3aa9f9cabaf73d3ee70818e8
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-14 02:21:09 +0700

    Editing Balance component :hammer:

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+import styles from '../styles';
 
 const Balance = () => {
-  return <div>Balance</div>;
+  const tokenBalance = parseUnits('0.23412');
+
+  return (
+    <div className={styles.balance}>
+      <p className={styles.balanceText}>
+        {tokenBalance && (
+          <>
+            <span className={styles.balanceBold}>Balance: </span>
+            {formatUnits(tokenBalance || parseUnits('0'))}
+          </>
+        )}
+      </p>
+    </div>
+  );
 };
 
 export default Balance;
