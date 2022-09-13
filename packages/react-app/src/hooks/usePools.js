3f4da1a04825decdea77a20ec24a46734d318e30
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-14 00:43:27 +0700

    Editing the utils Info :hammer:

--- packages/react-app/src/hooks/usePools.js
+++ packages/react-app/src/hooks/usePools.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 import { useConfig } from '@usedapp/core';
 
 import { ROUTER_ADDRESS } from '../config';
+import { getFactoryInfo, getRouterInfo } from '../utils';
 
 export const loadPools = async (providerUrl) => {
   const provider = new Web3.providers.HttpProvider(providerUrl);
   const web3 = new Web3(provider);
 
-  const routerInfo = null;
-  const factoryInfo = null;
+  const routerInfo = await getRouterInfo(ROUTER_ADDRESS, web3);
+  const factoryInfo = await getFactoryInfo(routerInfo.factory, web3);
 };
 
 export const usePools = () => {
--- packages/react-app/src/utils/getRouterInfo.js
+++ packages/react-app/src/utils/getRouterInfo.js
@@ -1,1 +1,9 @@
-export const getRouterInfo = async () => {};
+import { abis } from '@my-app/contracts';
+
+export const getRouterInfo = async (routerAddress, web3) => {
+  const router = new web3.eth.Contract(abis.router02, routerAddress);
+
+  return {
+    factory: await router.methods.factory().call(),
+  };
+};
