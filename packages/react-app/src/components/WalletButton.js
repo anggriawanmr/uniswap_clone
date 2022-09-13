410775d36a66704bc41eed9dbfb53cbc5f0c1daf
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-13 09:21:14 +0700

    Editing WalletButton component :hammer:

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { shortenAddress, useEthers, useLookupAddress } from '@usedapp/core';
+
+import styles from '../styles';
 
 const WalletButton = () => {
-  return <div>WalletButton</div>;
+  const [accountAddress, setAccountAddress] = useState('');
+  const { ens } = useLookupAddress();
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+
+  useEffect(() => {
+    if (ens) {
+      setAccountAddress(ens);
+    } else if (account) {
+      setAccountAddress(shortenAddress(account));
+    } else {
+      setAccountAddress('');
+    }
+  }, [account, ens, setAccountAddress]);
+
+  return (
+    <button
+      onClick={() => {
+        if (!account) {
+          activateBrowserWallet();
+        } else {
+          deactivate();
+        }
+      }}
+      className={styles.walletButton}
+    >
+      {accountAddress || 'Connect Wallet'}
+    </button>
+  );
 };
 
 export default WalletButton;
